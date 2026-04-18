9699cb125daebf9f31488731bfa6ae5acdcc59f2
CarlitosPJYT950 <juanestebanpradaospina@gmail.com>
2026-04-18 16:59:07 -0500

    Crear main.go

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+    name := "John Doe"
+    age := 24
+    weight := 154.61
+    isMale := true
+    fmt.Println(name)
+    fmt.Println(age)
+    fmt.Println(weight)
+    fmt.Println(isMale)
+}
